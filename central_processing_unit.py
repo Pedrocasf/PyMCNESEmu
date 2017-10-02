27ba6ad7a7fece4f7a4ec9554314376f82ea07cf
Pedrocasf <pedrostarling2000@gmail.com>
2017-10-02 16:15:10 -0300

    cpu rewrite almost done 2

--- central_processing_unit.py
+++ central_processing_unit.py
@@ -319,20 +319,26 @@ class CPU:
         if register < 0:
             register *= -1
         address = register
-
+        
+    def STP(self):
+        pass
+    
     def T(self, register_source, register_destination):
         register_destination = register_source
         self.value_zero_negative(register_destination)
 
-
-
-
+        
     def __init__(self):
 
-        self.opcode_table = {0x00:, 0x01:, 0x02:, 0x03:, 0x04:, 0x05:, 0x06:, 0x07:, 0x08:, 0x09:, 0x0A:, 0x0B:, 0x0C:, 0x0D:, 0x0E:, 0x0F:
-                            ,0x10:, 0x11:, 0x12:, 0x13:, 0x14:, 0x15:, 0x16:, 0x17:, 0x18:, 0x19:, 0x1A:, 0x1B:, 0x1C:, 0x1D:, 0x1E:, 0x1F:
-                            ,0x20:, 0x21:, 0x22:, 0x23:, 0x24:, 0x25:, 0x26:, 0x27:, 0x28:, 0x29:, 0x2A:, 0x2B:, 0x2C:, 0x2D:, 0x2E:, 0x2F:
-                            ,0x30:, 0x31:, 0x32:, 0x33:, 0x34:, 0x35:, 0x36:, 0x37:, 0x38:, 0x39:, 0x3A:, 0x3B:, 0x3C:, 0x3D:, 0x3E:, 0x3F:
+        self.opcode_table = {0x00:self.BRK,0x01:self.ORA(address=self.indexed_indirect()),0x02:self.STP,0x03:self.SLO(address=self.indexed_indirect()),0x04:self.NOP,0x05:self.ORA(address=self.zero_page()),0x06:self.ASL(address=zero_page())
+                            ,0x07:self.SLO(address=self.zero_page()),0x08:self.PHP,0x09:self.ORA(address=self.immediate()),0x0A:self.ASL(address=self.accumulator),0x0B:self.ANC(address=self.immediate()),0x0C:self.NOP,0x0D:self.ORA(address=self.absolute())
+                            ,0x0E:self.ASL(address=self.absolute()),0x0F:self.SLO(address=self.absolute()),0x10:self.BPL,0x11:self.ORA(adress=self.indirect_indexed()),0x12:self.STP,0x13:self.SLO(address=self.indirect_indexed()),0x14:self.NOP,
+                            0x15:self.ORA(address=self.zero_page_x()),0x16:self.ASL(address=self.zero_page_x()),0x17:self.SLO(address=self.zero_page_x()),0x18:self.CLC,0x19:self.ORA(address=self.absolute_y()),0x1A:self.NOP,0x1B:self.SLO(address=absolute_y())
+                            ,0x1C:self.NOP,0x1D:self.ORA(address=self.absolute_x()),0x1E:self.ASL(address=self.absolute_x()),0x1F:self.SLO(address=self.absolute_x()),0x20:self.JSR, 0x21:self.AND(address=self.indexed_indirect()),0x22:self.STP,
+                            0x23:self.RLA(address=self.indexed_indirect()),0x24:self.BIT(address=self.zero_page()),0x25:self.AND(address=self.zero_page()),0x26:self.ROL(address=zero_page()),0x27:self.RLA(address=self.zero_page()),0x28:self.PLP
+                            ,0x29:self.AND(address=self.immediate()),0x2A:self.ROL(address=self.accumulator),0x2B:self.ANC(address=self.immediate()),0x2C:self.BIT(address=self.absolute()),0x2D:self.AND(address=self.absolute()),0x2E:self.ROL(address=self.absolute())
+                            ,0x2F:self.RLA(address=self.absolute()),0x30:self.BMI,0x31:self.AND(address=self.indirect_indexed()),0x32:self.STP,0x33:self.RLA(address=self.indirect_indexed()),0x34:self.NOP,0x35:self.AND(address=zero_page_x()),
+                             0x36:self.ROL(address=self.zero_page_x()),0x37:self.RLA(address=self.zero_page_x()), 0x38:, 0x39:, 0x3A:, 0x3B:, 0x3C:, 0x3D:, 0x3E:, 0x3F:
                             ,0x40:, 0x41:, 0x42:, 0x43:, 0x44:, 0x45:, 0x46:, 0x47:, 0x48:, 0x49:, 0x4A:, 0x4B:, 0x4C:, 0x4D:, 0x4E:, 0x4F:
                             ,0x50:, 0x51:, 0x52:, 0x53:, 0x54:, 0x55:, 0x56:, 0x57:, 0x58:, 0x59:, 0x5A:, 0x5B:, 0x5C:, 0x5D:, 0x5E:, 0x5F:
                             ,0x60:, 0x61:, 0x62:, 0x63:, 0x64:, 0x65:, 0x66:, 0x67:, 0x68:, 0x69:, 0x6A:, 0x6B:, 0x6C:, 0x6D:, 0x6E:, 0x6F:
